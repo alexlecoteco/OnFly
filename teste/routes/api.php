89db216b8e7560bfca8221ef8b54dd8eb1095b9a
alexlecoteco <alexfernandogf@gmail.com>
2022-12-09 18:31:56 -0300

    Atualizando breeze para o api

--- teste/routes/api.php
+++ teste/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\DespesasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::apiResource('/despesas', DespesasController::class);
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if (Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+
+    $user = Auth::user();
+    $user->tokens()->delete();
+    $token = $user->createToken('token', ['is_admin']);
+
+    return response()->json($token->plainTextToken);
+});
