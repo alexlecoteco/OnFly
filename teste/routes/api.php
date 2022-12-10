1e4d8de47f605544a2c402d1df2bc35315d5fa93
alexlecoteco <alexfernandogf@gmail.com>
2022-12-10 11:26:49 -0300

    Inserindo rota de registro para a API

--- teste/routes/api.php
+++ teste/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\DespesasController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/despesas', DespesasController::class);
+Route::middleware('auth:sanctum')->group(function () { 
+    Route::apiResource('/despesas', DespesasController::class);
+});
+
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if (Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+
+    $user = Auth::user();
+    $user->tokens()->delete();
+    $token = $user->createToken('token', ['is_admin']);
+
+    return response()->json($token->plainTextToken);
+});
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+                ->middleware('guest')
+                ->name('register');
+
