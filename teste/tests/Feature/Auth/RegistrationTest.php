6bf5b8fc7bc82ad9626c4a30589d74218886afd4
alexlecoteco <alexfernandogf@gmail.com>
2022-12-11 10:19:49 -0300

    Refatorando o teste de registro de usuário

--- teste/tests/Feature/Auth/RegistrationTest.php
+++ teste/tests/Feature/Auth/RegistrationTest.php
@@ -19,14 +19,14 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
-        $response = $this->post('/register', [
+        $response = $this->withHeaders(['accept'=>'application/json'])
+            ->postJson('/api/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        $response->assertStatus(201);
     }
 }
